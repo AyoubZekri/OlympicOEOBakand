6ba751b69eec6c530c3b29295b409522cc5f4ceb
Zineb-web22 <bzineb296@gmail.com>
2026-08-18 17:13:48 +0100

    2026/08/18 17:13 Uesr and Role

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,10 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
+// Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::post('roles', \App\Http\Controllers\Api\RoleController::class);
     Route::post('users', \App\Http\Controllers\Api\UserController::class);
-});
+// });
