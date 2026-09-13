9960631715176e111ca7f4ae69dbd1dde65272f3
Zineb-web22 <bzineb296@gmail.com>
2026-09-13 14:08:36 +0100

    2026/09/13 6  Edit Some

--- app/Http/Controllers/Api/MatchCallupController.php
+++ app/Http/Controllers/Api/MatchCallupController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\MatchCallup;
-use App\Models\Individual;
 use Illuminate\Support\Facades\DB;
 
 class MatchCallupController extends Controller
